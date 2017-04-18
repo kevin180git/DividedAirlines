c7fa7b875346acb794d35483681faa19f5e2f506
brianwang9100@gmail.com <brianwang9100@gmail.com>
2017-04-18 03:06:12 -0400

    finished edit

---
+++ DividedAirlines/MainMenu.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenu.swift
+//  DividedAirlines
+//
+//  Created by Brian Wang on 4/17/17.
+//  Copyright © 2017 Kevin Lee. All rights reserved.
+//
+
+import UIKit
+
+class MainMenu: SKScene {
+
+}
